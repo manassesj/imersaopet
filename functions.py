c31970fb963595d6bcc5f3997d95cc679b6fa131
manassesj <jmanasses1024@hotmail.com>
2021-10-21 15:26:40 -0300

    #2

--- functions.py
+++ functions.py
@@ -5,3 +5,17 @@ def count_genre(genres, dataframe):
         result.append(count)
     
     return result
+
+def data_result_all_years_by_gender(data, gender):
+    data_result = data.loc[data.gender == gender] \
+        .groupby(data.release_date.dt.year).size()\
+            .reset_index(name="times")
+    
+    return data_result
+
+def data_result_by_set_of_years_by_gender(data, gender, years):
+    data_result = data.loc[(data.gender == gender) & data.release_date.dt.year.isin(years)]\
+        .groupby(data.release_date.dt.year).size()\
+            .reset_index(name="times")
+    
+    return data_result
