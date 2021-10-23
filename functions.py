987e3a4fb521f8ac472146727a30b791b76a7280
manassesj <jmanasses1024@hotmail.com>
2021-10-23 16:52:37 -0300

    #3

--- functions.py
+++ functions.py
@@ -1,3 +1,24 @@
+import pandas as pd
+from column_names import get_movie_colum_names, get_rating_colum_names, get_user_colum_names
+
+def get_all_data():
+    
+    enconding = "ISO-8859-1"
+    movie_file = "./ml-100k/u.item"
+    rating_file = "./ml-100k/u.data"
+    user_file = "./ml-100k/u.user"
+
+    data_movie = pd.read_csv(movie_file, encoding='ISO-8859-1',
+                            sep="|", names=get_movie_colum_names())
+
+    data_rating = pd.read_csv(rating_file, encoding='ISO-8859-1',
+                            sep="\t", names=get_rating_colum_names())
+
+    data_user = pd.read_csv(user_file, encoding='ISO-8859-1',
+                        sep="|", names=get_user_colum_names())
+
+    return [data_movie, data_rating, data_user]
+
 def count_genre(genres, dataframe):
     result = []
     for genre in genres:
